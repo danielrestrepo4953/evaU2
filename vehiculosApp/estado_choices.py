4f88e92069e3d4e6d21190d870e517ab8f028afb
Daniel <daniel.restrepo@inacapmail.cl>
2026-09-23 13:02:25 -0300

    Creado superusuario, creadas clases de vehiculosApp e implementados los cambios en la base de datos, preparativos para poblar la BD y pasar los datos a las vistas de vehiculosApp

---
+++ vehiculosApp/estado_choices.py
@@ -0,0 +1,6 @@
+estados = (
+    ('Bueno', 'En buen estado'),
+    ('Intermedio', 'Condiciones intermedias'),
+    ('Malas', 'En malas condiciónes')
+
+)
--- vehiculosApp/models.py
+++ vehiculosApp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+from django.utils import timezone
+from vehiculosApp.estado_choices import estados
 # Create your models here.
+class Vehiculo(models.Model):
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    anio = models.IntegerField()
+    patente = models.CharField(max_length=10, unique=True)
+    imagen = models.ImageField(upload_to='vehiculos/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} ({self.anio})"
+
+    class Meta:
+        db_table = "vehiculos"
+        verbose_name = "Vehículo"
+        verbose_name_plural = "Vehículos"
+        ordering = ['marca', 'modelo', 'anio']
+
+class Detalle(models.Model):
+    vehiculo = models.ForeignKey(Vehiculo, on_delete=models.CASCADE, related_name='detalle')
+    color = models.CharField(max_length=50)
+    tipo_motor = models.CharField(max_length=50)
+    tipo_caja = models.CharField(max_length=50)
+    fecha_ultimo_servicio = models.DateField(default=timezone.now, blank=True, null=True)
+    kilometraje = models.IntegerField()
+    estado = models.CharField(max_length=50, choices=estados, verbose_name="Estado del vehículo")
+    observaciones = models.TextField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return f"Detalle de {self.vehiculo.marca} {self.vehiculo.modelo} ({self.vehiculo.anio})"
+
+    class Meta:
+        db_table = "detalles_vehiculos"
+        verbose_name = "Detalle del Vehículo"
+        verbose_name_plural = "Detalles de Vehículos"
+        ordering = ['vehiculo__marca', 'vehiculo__modelo', 'vehiculo__anio']
