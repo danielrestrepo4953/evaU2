ba5d8ebd1bf7718e251d8a7f1095c450737250af
Daniel <daniel.restrepo@inacapmail.cl>
2026-09-24 20:11:50 -0300

    variables de entorno

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env.example')
+load_dotenv(BASE_DIR / '.env')
 TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 
 
